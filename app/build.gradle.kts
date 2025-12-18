5bb69e66631b21c4152dbeb92ca75fa631f1adf5
dhiya <dhiyanajwaatma1227@gmail.com>
2025-12-18 22:38:57 +0800

    firebase setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -57,4 +58,11 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation("com.google.firebase:firebase-auth-ktx") //auth
+    implementation("com.google.firebase:firebase-firestore-ktx") // firestore
+    implementation("com.google.firebase:firebase-messaging") // notif
+    implementation("com.google.firebase:firebase-analytics-ktx") //analytics
 }
